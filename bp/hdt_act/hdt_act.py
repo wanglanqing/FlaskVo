cee87db26afb8bf803468a31a1f6f96eb7930005
wanglanqing <wanglanqing1122@126.com>
2019-01-29 14:46:19 +0800

    1. 活动相关功能模块化

--- bp/hdt_act/hdt_act.py
+++ bp/hdt_act/hdt_act.py
@@ -2,9 +2,17 @@
 # @Time    : 2019/1/28 9:52
 # @Author  : wanglanqing
 
+import traceback
 from flask import Blueprint, request, render_template
 from business_modle.relate.adzoneActForm import adzoneActForm
 from business_modle.relate.relateAdzoneAds import relateAdzoneAct
+from business_modle.templateToAct.templateAct import templateAct
+from business_modle.templateToAct.templateActForm import templateActForm
+from business_modle.checkRoute.checkRouteForm import *
+from business_modle.checkRoute.checkRoute import checkNodeRoute
+from business_modle.querytool.create_template import *
+from business_modle.querytool.myException import *
+
 #创建活动蓝图
 act = Blueprint('act', __name__,template_folder='templates')
 
@@ -22,3 +30,99 @@ def adzoneAct():
         link = RAA.get_link()
         adzone =  RAA.get_adzone_url()
         return render_template('adzoneAct.html', form=form, pos=1, link=link, adzone=adzone)
+
+#活动模板互查工具
+@act.route('/templateToAct/<any(query,position):page_name>/',methods=['get','post'])
+def templateToAct(page_name):
+    form=templateActForm()
+    if request.method=='GET' and page_name=='query':
+        return render_template('template/templateToAct.html', ts='false', form=form)
+    elif request.method == 'POST' and page_name == 'query':
+        act_ids = request.form.get('ad_ids')
+        env = request.form.get('env')
+        template_kws = request.form.get('template_kws')
+        tta = templateAct(env)
+        if template_kws:
+            template_kws = template_kws.encode('utf-8')
+        re = tta.get_infos(template_kws, act_ids)
+        if isinstance(re,list) and len(re)>0:
+            tta.exportTemplateXls(re)
+            return render_template('template/templateToAct.html', ts='true', form=form,re=re,env=env, flag='true')
+        else:
+            return render_template('template/templateToAct.html', ts='true', form=form,re=re,env=env, flag='flase')
+    elif page_name == 'position' and request.method == 'GET':
+        position_id = request.args.get('id')
+        env_tmp = request.args.get('env')
+        tta2 = templateAct(env_tmp)
+        position_re = tta2.get_position(position_id)
+        return render_template('template/position.html', re=position_re)
+
+#活动检查路由
+@act.route('/checkRoute/',methods=['get','post'])
+def checkRoute():
+    form = checkRouteForm()
+    re = ''
+    if request.method == 'GET':
+        return render_template('checkRoute.html',form = form,re=re)
+    else:
+        datas = form.data
+        env = datas['env'].strip()
+        adzoneLink = datas['adzoneLink'].strip()
+        cr = checkNodeRoute(env,adzoneLink)
+        re = cr.join_url()
+        if isinstance(re,list) and len(re)>0:
+            re_len = len(re)
+            return render_template('checkRoute.html', form=form, re=re, re_type=1,re_len=re_len)
+        else:
+            return render_template('checkRoute.html',form = form,re=re,re_type=0)
+
+#创建活动
+@act.route('/create_act/', methods=['POST','GET'])
+def create_act():
+    if request.method=='GET':
+        return render_template("create_act.html", template_adr='1111')
+    else:
+        template_adr= request.form.get('template_adr').strip()
+        css_adr =  request.form.get('css_adr').strip()
+        template_type_name=request.form.get('template_type_name').strip()
+        temlate_name=request.form.get('temlate_name').strip()
+        #增加模板配置字段
+        template_conf_items= str(request.form.get('template_conf_items')).strip()
+        act_name=request.form.get('act_name').strip()
+        award_num =int(request.form.get('award_num'))
+        free_num = int(request.form.get('free_num').strip())
+        request.accept_charsets
+        try:
+            ct = TemplateActCreation(template_type_name, act_name,award_num)
+            # 创建模板类型，create_template_type(self, classifi, locationAdress, preview="https://img0.adhudong.com/template/201802/24/999337a35a1a9169450685cc66560a05.png",prizesNum=6)
+            template_type_re = ct.create_template_type(template_adr)
+            if template_type_re.json()['code'] == 200:
+                template_type_fe = '创建模板类型【' +template_type_name + '】,成功了，返回结果是: \n' + template_type_re.text
+            else:
+                raise myException('create_template_type ', template_type_re.text)
+
+            # 创建模板 ct.create_template(templateName, templateStyleUrl)
+            temlate_name_re = ct.create_template(temlate_name, css_adr, template_conf_items=template_conf_items)
+            # print(temlate_name_re)
+            if temlate_name_re.json()['code'] == 200:
+                temlate_name_fe = '创建模板【' + temlate_name + '】,成功了，返回结果是: \n' + temlate_name_re.text
+            else:
+                raise myException('create_template ', temlate_name_re)
+
+            # # 创建活动，create_act(self, act_name,free_num=20, award_num=6)
+            act_re = ct.create_act(free_num)
+            # print(act_re)
+            if act_re.json()['code'] == 200:
+                act_fe = '创建活动【' + act_name + '】,成功了，返回结果是: \n' + act_re.text
+            else:
+                raise myException('create_act ', act_re.text)
+
+            # # 创建活动关联的奖品，
+            awards_re = ct.create_awards()
+            # print(awards_re)
+             ##关联广告位
+            adzone_re = ct.adzone_act()
+            return render_template("create_act.html", template_type_re=template_type_fe, temlate_name_re=temlate_name_fe , act_re=act_fe, awards_re =awards_re ,adzone_re=adzone_re)
+        except Exception as e:
+            traceback.print_exc()
+            return render_template("create_act.html", f_re = e.message)
--- show.py
+++ show.py
@@ -2,12 +2,9 @@
 #encoding: utf-8
 #version:2018/07/10,增加接口统计视图函数
 import os
-import traceback
-
 from flask import jsonify
-from flask import Flask,request,render_template,flash
-# from flask_paginate import Pagination,get_page_parameter
-
+from flask import Flask,request,render_template,flash,url_for
+from flask_paginate import Pagination,get_page_parameter
 from business_modle.report import hdtmonitor as m
 from business_modle.testtools import hdt_cssc as cssc
 from business_modle.report import reportdata as r
@@ -16,8 +13,6 @@ from business_modle.launch import launchlistdb as lc
 from business_modle.querytool import bidding_analysis as ba
 from business_modle.querytool import myredis as mr
 from business_modle.querytool import myredis_status as mrs
-from business_modle.querytool.create_template import *
-from business_modle.querytool.myException import *
 from business_modle.querytool.confparas import *
 from business_modle.querytool.get_act import *
 from business_modle.querytool.updateAdId import *
@@ -42,10 +37,6 @@ from utils.Emar_SendMail_Attachments import *
 from config import mail_template,sqls
 from business_modle.Crm.CrmOrderEffectCheck import Crm
 from business_modle.querytool.phoneVaildCode import *
-from business_modle.checkRoute.checkRouteForm import *
-from business_modle.checkRoute.checkRoute import checkNodeRoute
-from business_modle.templateToAct.templateAct import templateAct
-from business_modle.templateToAct.templateActForm import templateActForm
 from bp.hdt_act.hdt_act import act
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
@@ -301,9 +292,6 @@ def myredis_status():
         order_status,allcount,negativecount=mrs.mygetredis(redis_nodes)
         return render_template('myredis_status.html',order_status=order_status,allcount=allcount,negativecount=negativecount,data=data)
 
-
-
-
 @app.route('/voyagerlog/',methods=('POST','GET'))
 def voyagerlog():
     zclk=request.form.get('zclk')
@@ -382,58 +370,6 @@ def getlanuch():
     id=request.args.get('id')
     result=lc.getlanuch(id)
     return render_template('launchdetial.html',result=result)
-
-@app.route('/create_act/', methods=['POST','GET'])
-def create_act():
-    if request.method=='GET':
-        return render_template("create_act.html", template_adr='1111')
-    else:
-        template_adr= request.form.get('template_adr').strip()
-        css_adr =  request.form.get('css_adr').strip()
-        template_type_name=request.form.get('template_type_name').strip()
-        temlate_name=request.form.get('temlate_name').strip()
-        #增加模板配置字段
-        template_conf_items= str(request.form.get('template_conf_items')).strip()
-        act_name=request.form.get('act_name').strip()
-        award_num =int(request.form.get('award_num'))
-        free_num = int(request.form.get('free_num').strip())
-        request.accept_charsets
-        try:
-            ct = TemplateActCreation(template_type_name, act_name,award_num)
-            # 创建模板类型，create_template_type(self, classifi, locationAdress, preview="https://img0.adhudong.com/template/201802/24/999337a35a1a9169450685cc66560a05.png",prizesNum=6)
-            template_type_re = ct.create_template_type(template_adr)
-            if template_type_re.json()['code'] == 200:
-                template_type_fe = '创建模板类型【' +template_type_name + '】,成功了，返回结果是: \n' + template_type_re.text
-            else:
-                raise myException('create_template_type ', template_type_re.text)
-
-            # 创建模板 ct.create_template(templateName, templateStyleUrl)
-            temlate_name_re = ct.create_template(temlate_name, css_adr, template_conf_items=template_conf_items)
-            print(temlate_name_re)
-            if temlate_name_re.json()['code'] == 200:
-                temlate_name_fe = '创建模板【' + temlate_name + '】,成功了，返回结果是: \n' + temlate_name_re.text
-            else:
-                # raise myException('create_template ', temlate_name_re.text)
-                raise myException('create_template ', temlate_name_re)
-
-            # # 创建活动，create_act(self, act_name,free_num=20, award_num=6)
-            act_re = ct.create_act(free_num)
-            print(act_re)
-            if act_re.json()['code'] == 200:
-                act_fe = '创建活动【' + act_name + '】,成功了，返回结果是: \n' + act_re.text
-            else:
-                raise myException('create_act ', act_re.text)
-
-            # # 创建活动关联的奖品，
-            awards_re = ct.create_awards()
-            print(awards_re)
-            # return render_template("create_act.html",  act_re=template_type_fe, awards_re =awards_re )
-             ##关联广告位
-            adzone_re = ct.adzone_act()
-            return render_template("create_act.html", template_type_re=template_type_fe, temlate_name_re=temlate_name_fe , act_re=act_fe, awards_re =awards_re ,adzone_re=adzone_re)
-        except Exception as e:
-            traceback.print_exc()
-            return render_template("create_act.html", f_re = e.message)
 
 @app.route('/confparas/', methods=['POST','GET'])
 def confparas():
@@ -602,8 +538,6 @@ def Api_index():
     re = at.query_api_stat_summary()
     return render_template('TestCase/apiStatic.html',re =re, col_len = len(re[0]))
 
-
-
 @app.route('/test/')
 def test():
     return render_template("test.html")
@@ -613,12 +547,9 @@ def myiframe():
 
 @app.route('/version_maintain/<any(new,update):page_name>/',methods=('POST','GET'))
 def version_maintain(page_name):
-    # mail = Mail(app)
     form = VersionTrackerForm()
     vt = VersionTracker()
     if page_name == 'new':
-    # form = VersionTrackerForm()
-    # vt = VersionTracker()
         if form.is_submitted():
             sql_data = form.data
             print sql_data
@@ -663,7 +594,6 @@ def version_maintain(page_name):
         id = request.form.get('id')
         status = request.form.get('status')
         v_desc = request.form.get('v_desc')
-        # job_name = request.form.get('job_name')
         tester = request.form.get('tester').strip().replace('，',',')
         re=vt.update_version_desc_state(id,status,v_desc,tester)
         if int(re) != 0:
@@ -675,14 +605,11 @@ def version_maintain(page_name):
             result=lc.getlanuch(id)
             return render_template("launchdetial.html",result=result)
 
-
 @app.route('/recharge',methods=['POST','GET'])
-
 def recharge():
     title=u'测试环境模拟联动充值'
     if request.method=='GET':
         return render_template('recharge.html',title=title)
-
     else:
         advertiser_id = request.form.get('advertiser_id')
         amount=request.form.get('amount')
@@ -701,7 +628,6 @@ def recharge():
         return render_template("recharge.html",result=result,title=title,paras=paras)
 
 @app.route('/crm_order',methods=['POST','GET'])
-
 def crm_order():
 
     title=u'优品购订单统计'
@@ -738,10 +664,7 @@ def cpa_api():
 def render_link():
     title = u'广告主数据收集(生成链接)'
     if request.method == 'GET':
-
         return render_template('advertiser_test.html',title=title)
-
-
     else:
         ad_link = request.form.get('url')
         result1 = Advertiser_collect(ad_link,env_value=False)
@@ -758,10 +681,7 @@ def render_link():
 def query_result():
     title = u'广告主数据收集(查询结果)'
     if request.method == 'GET':
-
         return render_template('advertiser_test.html',title=title)
-
-
     else:
         ad_link = request.form.get('url')
         ad_click_tag = request.form.get('ad_click_tag')
@@ -865,25 +785,6 @@ def phoneVaildCode():
         re = pp.get_valid_code()
         return  render_template('phoneVaildCode.html',re=re,pos=env[0])
 
-@app.route('/checkRoute/',methods=['get','post'])
-def checkRoute():
-    form = checkRouteForm()
-    re = ''
-    if request.method == 'GET':
-        return render_template('checkRoute.html',form = form,re=re)
-    else:
-        datas = form.data
-        env = datas['env'].strip()
-        adzoneLink = datas['adzoneLink'].strip()
-        cr = checkNodeRoute(env,adzoneLink)
-        re = cr.join_url()
-        if isinstance(re,list) and len(re)>0:
-            re_len = len(re)
-            return render_template('checkRoute.html', form=form, re=re, re_type=1,re_len=re_len)
-        else:
-            return render_template('checkRoute.html',form = form,re=re,re_type=0)
-
-
 @app.route('/punchcard',methods=['POST','GET'])
 def punchcard():
     title = u"小程序用户信息"
@@ -903,32 +804,6 @@ def punchcard():
         xvalue=punchcard_result.dateRange(begin_date,end_date)
         data=punchcard_result.add_user(begin_date,end_date)
         return render_template('punchcard.html',total_amount=total_amount,today_addamount=total_addamount,today_sign=today_sign,begin_date=begin_date,end_date=end_date,xvalue=xvalue,data=data,invite_add=invite_add,non_inviteadd=non_inviteadd)
-
-
-@app.route('/templateToAct/<any(query,position):page_name>/',methods=['get','post'])
-def templateToAct(page_name):
-    form=templateActForm()
-    if request.method=='GET' and page_name=='query':
-        return render_template('template/templateToAct.html', ts='false', form=form)
-    elif request.method == 'POST' and page_name == 'query':
-        act_ids = request.form.get('ad_ids')
-        env = request.form.get('env')
-        template_kws = request.form.get('template_kws')
-        tta = templateAct(env)
-        if template_kws:
-            template_kws = template_kws.encode('utf-8')
-        re = tta.get_infos(template_kws, act_ids)
-        if isinstance(re,list) and len(re)>0:
-            tta.exportTemplateXls(re)
-            return render_template('template/templateToAct.html', ts='true', form=form,re=re,env=env, flag='true')
-        else:
-            return render_template('template/templateToAct.html', ts='true', form=form,re=re,env=env, flag='flase')
-    elif page_name == 'position' and request.method == 'GET':
-        position_id = request.args.get('id')
-        env_tmp = request.args.get('env')
-        tta2 = templateAct(env_tmp)
-        position_re = tta2.get_position(position_id)
-        return render_template('template/position.html', re=position_re)
 
 @app.route('/mini_mediainfo',methods=['POST','GET'])
 def mini_mediainfo():
@@ -962,8 +837,6 @@ def mini_userinfo():
         mini_info= Mini_userinfo(nickname,env_value=False)
         paras=mini_info.userinfo()
         return render_template('mini_userinfo.html',nick_name=nickname,paras=paras)
-
-
 
 if __name__ == '__main__':
     app.run( host="0.0.0.0",port=21312,debug=True)
