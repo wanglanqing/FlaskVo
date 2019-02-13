41a6cf04d0068a8595a5d0bf08d5a9b667250428
wanglanqing <wanglanqing1122@126.com>
2019-02-13 14:41:05 +0800

    1. 优化构造json串的方法

--- utils/ToBeJson.py
+++ utils/ToBeJson.py
@@ -2,6 +2,7 @@
 # @Time    : 2018/10/1 11:05
 # @Author  : wanglanqing
 
+import json
 
 class ToBeJson(object):
     def __init__(self,keys,values):
@@ -16,12 +17,14 @@ class ToBeJson(object):
         if keys_len != len(values[0]):
             raise "参数个数不一致"
 
-        for iv in range(values_len):
-            tmp_dict = {}
-            for i in range(keys_len):
-                tmp_dict[keys[i]]=values[iv][i]
-            data.append(tmp_dict)
+        # for iv in range(values_len):
+        #     tmp_dict = {}
+        #     for i in range(keys_len):
+        #         tmp_dict[keys[i]]=values[iv][i]
+        #     data.append(tmp_dict)
+        list_json = [dict(zip(keys,value)) for value in values]
+        data = json.dumps(list_json,ensure_ascii=False,indent=2)
         return data
 
 if __name__=='__main__':
-    ToBeJson.trans(['id','name','money'],(('1','sd','22'),('2','gg','33')))
+    print ToBeJson.trans(['id','name','money'],(('1','sd','22'),('2','gg','33')))
